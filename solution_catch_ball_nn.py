e35717e649ece92c5e51b8455a8192fead43b632
WHJR-Grade12 <91465322+WHJR-G12-Github@users.noreply.github.com>
2021-09-29 11:21:47 +0530

    Update solution_catch_ball_nn.py

--- solution_catch_ball_nn.py
+++ solution_catch_ball_nn.py
@@ -1,16 +1,16 @@
 import pygame, sys, random
 import neat
 
-#initialize pygame to  use it's functions
+# Initializing pygame to  use it's functions
 pygame.init()
 clock=pygame.time.Clock()
 
-#create a window where game will Run
+# Creating a window where game will Run
 screen = pygame.display.set_mode((400,400))
-#title 
+# Setting title for the game
 pygame.display.set_caption("Catch the Ball")
 
-#load images
+# Loading images
 background_image = pygame.image.load("bg.png").convert()
 plr_img = pygame.image.load("player.png").convert_alpha()
 plr_img=pygame.transform.smoothscale(plr_img,(60,90))
@@ -19,7 +19,7 @@ ball_image=pygame.transform.smoothscale(ball_image,(40,40))
 over_img=pygame.image.load("over.png").convert_alpha()
 over_img=pygame.transform.smoothscale(over_img,(200,100))
 
-#creating objects of game
+# Creating objects of game
 ball=pygame.Rect(200,0,40,40)
 player=pygame.Rect(100,310,60,90)
 
@@ -37,6 +37,8 @@ def eval_genomes(genomes, config): #######
 
         count = 0
 
+        # Creating a 'FeedForwardNetwork' using 'create()' function
+        # Passing 'genome' and 'config' as parameters to 'create()'
         net = neat.nn.FeedForwardNetwork.create(genome, config)
 
         count_font=pygame.font.Font('freesansbold.ttf', 20)
@@ -50,12 +52,14 @@ def eval_genomes(genomes, config): #######
             genome.fitness+=0.1
 
             screen.blit(background_image,[0,0])
-            #event loop to check which key is print
+            
+            # Checking if 'quit' event occurs
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-                #use events to move the player
+                    
+                # Checking for events and handling moveents accordingly
                 if event.type==pygame.KEYDOWN:
                     if event.key==pygame.K_RIGHT:
                         speed=5
@@ -64,14 +68,19 @@ def eval_genomes(genomes, config): #######
 
             player.x=player.x+speed
 
+            # Calculating the output and decision making
+            # Calculating the ouput using 'activate()' function
+            # Passing x-coordinates of ball and player as arguments to 'activate()'
             output = net.activate((ball.x, player.x))   
             if output[0] > 0.5:
+               # Assigning the value of 'speed' variable as 5
                speed=5
 
             else:
+               # Assign the value of 'speed' variable as -5
                speed=-5
 
-            #code for ball falling
+            # Code for ball falling
             ball.y=ball.y+5
             
             if(ball.colliderect(player)):
